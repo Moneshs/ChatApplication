2b4023e4dbb7e68d49114e2e0e7c976f3a0befc9
Moneshs <moneshsundar19@gmail.com>
2020-02-06 10:08:10 +0000

    [Modify]Monesh

--- scripts/After_install.sh
+++ scripts/After_install.sh
@@ -4,7 +4,7 @@ cd /home/ubuntu/ChatApplication
 sudo virtualenv venv
 source venv/bin/activate
 cd chat
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 cd ..
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sudo rm -f /home/ubuntu/ChatApplication/chat
+sudo rm -rf /home/ubuntu/ChatApplication/chat
