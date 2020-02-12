e7187afd33a603311196f6bb98878361ce887cab
Monesh <58931303+Moneshs@users.noreply.github.com>
2020-02-12 12:07:58 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
 
-rm -rf /home/ubuntu/ChatApplication/chat/*
-rm -rf /home/ubuntu/ChatApplication/scripts/*
-rm -rf /home/ubuntu/ChatApplication/appspec.yml
+rm -rf /home/ubuntu/ChatApplication/*
