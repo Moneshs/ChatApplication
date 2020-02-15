60290cd16bb36a2eaa655613c8f6998b04f96b0d
Monesh <58931303+Moneshs@users.noreply.github.com>
2020-02-15 09:54:44 +0530

    Update settings.py

--- chat/chatapp/settings.py
+++ chat/chatapp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4tz_w)booce3w)fm!k!%sp9c3$a#)cr6s+4*e^#^8^ko8@1p&9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False
+DEBUG =True
 
 ALLOWED_HOSTS = ['*']
 
